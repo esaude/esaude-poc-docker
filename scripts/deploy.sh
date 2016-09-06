8000a99e16187cafc6ec9cc92d1a1b64edf05bea
Pascal Brandt <psbrandt@gmail.com>
2016-09-06 14:02:58 +0200

    💾 Added Bintray deploy for offline install.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -o xtrace
 
 # Log in to the Bintray Docker registry
-docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY -e $BINTRAY_EMAIL esaude-docker-poc-docker.bintray.io
+docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY esaude-docker-poc-docker.bintray.io
+
+set -o xtrace
 
 # Tag the image
 docker tag `docker images -q esaude-emr-poc` esaude-docker-poc-docker.bintray.io/poc:$POC_VERSION
@@ -10,6 +11,17 @@ docker tag `docker images -q esaude-emr-poc` esaude-docker-poc-docker.bintray.io
 # Push the image
 docker push esaude-docker-poc-docker.bintray.io/poc:$POC_VERSION
 
+# Bundle app for offline install
+docker save -o esaude-app-poc.tar esaude-docker-poc-docker.bintray.io/poc
+gzip esaude-app-poc.tar
+
 # Notify to slack
-SLACK_MESSAGE="New eSaude POC Docker image ($POC_VERSION) published <https://bintray.com/esaude/poc-docker/poc|here>"
+if [ "$POC_VERSION" = "master" ];
+then
+  # build from master
+  SLACK_MESSAGE="New eSaude POC <https://bintray.com/esaude/poc-docker/poc|Docker image> published ($POC_VERSION)"
+else
+  SLACK_MESSAGE="New eSaude POC <https://bintray.com/esaude/poc-docker/poc|Docker image> and <https://bintray.com/esaude/apps/esaude-app-poc|offline installer> published ($POC_VERSION)"
+fi
+
 curl -X POST --data-urlencode 'payload={"username": "eSaude Bintray", "text": "'"$SLACK_MESSAGE"'", "icon_url": "https://bintray.com/assets/favicon.png"}' $SLACK_WEBHOOK_URL
